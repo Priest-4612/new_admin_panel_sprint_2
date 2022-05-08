ff079cd7c0e48e1aa7ddc97a4d536a897ab2eb63
Priest4612 <priest4612@gmail.com>
2022-05-08 23:56:07 +0300

    Изменил код скрипта заполнения бд

--- 01_docker_compose/app/service/loader_sqlite_to_postgres/sqlite_loader.py
+++ 01_docker_compose/app/service/loader_sqlite_to_postgres/sqlite_loader.py
@@ -3,10 +3,11 @@ class SQLiteLoader(object):
         self._cursor = cursor
 
     def load_data(self, table, dataclass_dict, load_rows_size=1):
+        dataclass = dataclass_dict[table]
         query = 'SELECT * FROM {table};'.format(table=table)
         self._cursor.execute(query)
         while True:
             loaded_data = self._cursor.fetchmany(load_rows_size)
             if not loaded_data:
                 break
-            yield [dataclass_dict[table](**row) for row in loaded_data]
+            yield [dataclass(**row) for row in loaded_data]
